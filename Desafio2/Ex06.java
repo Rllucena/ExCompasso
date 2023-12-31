3f1303776670c086e26e2c38c42680167b267e8f
aarrieche <arriechealexandre@gmail.com>
2023-12-31 19:49:04 -0300

    :sparkles: feat: adicionando exercicios 1 e 6

---
+++ Desafio2/Ex06.java
@@ -0,0 +1,37 @@
+/*
+Desenvolva um programa que determine se um número é um
+palíndromo especial.
+ */
+
+import java.util.Scanner;
+
+public class Ex06 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        String numeroStr;
+        String invertidaStr;
+        
+
+        System.out.println("Digite o numero: ");
+        numero = scanner.nextInt();
+
+        // converte para string
+        numeroStr = Integer.toString(numero);
+
+        // inverte a string
+        invertidaStr = new StringBuilder(numeroStr).reverse().toString();
+
+
+        if (invertidaStr.equals(numeroStr)) {
+            System.out.println("O numero " + numero + " é um palindromo especial");
+        }
+        else {
+            System.out.println("o numero " + numero + " nao é um palindromo especial");
+        }
+
+        scanner.close();
+    }
+}
