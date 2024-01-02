9a67a9293ebb441c4baf354582b677a007e1d074
unknown <abreumarcos13462@gmail.com>
2024-01-02 19:03:47 -0300

    s

--- Desafio2/Ex08.java
+++ Desafio2/Ex08.java
@@ -1,13 +1,13 @@
 
 //Elabore um programa que converta um número decimal para binário.
 
-package ex08;
+
 
 public class Ex08 {
 
 	public static void main(String[] args) {
 		
-		double decimal = 10.11;
+		double decimal = 10;
 		String s = Double.toString(decimal);
         
         s = s.replace('.', ' ');
